18eb12bf8696af336673a7d03df9b2a0bdfc9e4d
AquaUseful <l.vladik.m@gmail.com>
2023-01-22 17:05:04 +0300

    Initial commit

---
+++ src/main/java/org/psu/lab567/App.java
@@ -0,0 +1,18 @@
+package org.psu.lab567;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
+}
