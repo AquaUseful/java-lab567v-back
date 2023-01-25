d00403d1f49ec47f69d5e9ae34e5294b79af61c3
AquaUseful <l.vladik.m@gmail.com>
2023-01-26 01:56:38 +0300

    add: User model class

--- src/main/java/org/psu/lab567/model/User.java
+++ src/main/java/org/psu/lab567/model/User.java
@@ -1,5 +1,7 @@
 package org.psu.lab567.model;
 
+import java.util.Collection;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -7,6 +9,10 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -27,7 +33,10 @@ public class User {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = true)
-    @JoinColumn
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToOne(optional = true)
+    @JoinColumn(name = "avatar")
     private BinFile avatar;
 }
