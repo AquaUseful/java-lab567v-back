540e551830a2dfb7e866b12ff961008704ec9400
AquaUseful <l.vladik.m@gmail.com>
2023-01-28 14:57:48 +0300

    add: User repo methods

--- src/main/java/org/psu/lab567/repository/UserRepository.java
+++ src/main/java/org/psu/lab567/repository/UserRepository.java
@@ -1,9 +1,16 @@
 package org.psu.lab567.repository;
 
+import java.util.Optional;
+
 import org.psu.lab567.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import lombok.NonNull;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> getByEmail(@NonNull String email);
+
+    boolean existsByEmail(@NonNull String email);
 }
