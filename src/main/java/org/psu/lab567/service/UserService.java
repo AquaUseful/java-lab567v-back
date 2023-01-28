f2be3202490e75c5b707be4ef2d5a66c05d7f2a5
AquaUseful <l.vladik.m@gmail.com>
2023-01-28 14:46:27 +0300

    add: Some methods for user service

--- src/main/java/org/psu/lab567/service/UserService.java
+++ src/main/java/org/psu/lab567/service/UserService.java
@@ -1,11 +1,13 @@
 package org.psu.lab567.service;
 
-import javax.validation.constraints.NotNull;
+import javax.persistence.EntityNotFoundException;
 
 import org.psu.lab567.model.User;
 import org.psu.lab567.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import lombok.NonNull;
 
 @Service
 public class UserService {
@@ -13,9 +15,23 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public User getById(@NotNull Long id) {
+    public User getById(@NonNull Long id) {
         final User user = userRepository.getReferenceById(id);
         return user;
+    }
+
+    public User getByEmail(@NonNull String email) {
+        final User user = userRepository.getByEmail(email).orElseThrow(
+                () -> new EntityNotFoundException());
+        return user;
+    }
+
+    public boolean existsByEmail(@NonNull String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    public void save(@NonNull User user) {
+        userRepository.save(user);
     }
 
 }
