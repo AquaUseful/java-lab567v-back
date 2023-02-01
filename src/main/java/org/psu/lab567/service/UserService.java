036f8c791f802e201702f682f691ebd6ba757546
AquaUseful <l.vladik.m@gmail.com>
2023-02-01 14:12:04 +0300

    fix: Fix nosession error

--- src/main/java/org/psu/lab567/service/UserService.java
+++ src/main/java/org/psu/lab567/service/UserService.java
@@ -1,11 +1,14 @@
 package org.psu.lab567.service;
 
+import java.util.Collection;
+
 import javax.persistence.EntityNotFoundException;
 
 import org.psu.lab567.model.User;
 import org.psu.lab567.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.NonNull;
 
@@ -16,7 +19,7 @@ public class UserService {
     private UserRepository userRepository;
 
     public User getById(@NonNull Long id) {
-        final User user = userRepository.getReferenceById(id);
+        final User user = userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException());
         return user;
     }
 
@@ -32,6 +35,11 @@ public class UserService {
 
     public void save(@NonNull User user) {
         userRepository.save(user);
+    }
+
+    public Collection<User> getAll() {
+        final Collection<User> users = userRepository.findAll();
+        return users;
     }
 
 }
