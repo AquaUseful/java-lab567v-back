c27fd0fcd1aa9aa779a8e6bc668a770ff0192eb9
AquaUseful <l.vladik.m@gmail.com>
2023-02-01 14:13:35 +0300

    fix: Change message for unexistent user

--- src/main/java/org/psu/lab567/exception/UserExistsException.java
+++ src/main/java/org/psu/lab567/exception/UserExistsException.java
@@ -1,7 +1,7 @@
 package org.psu.lab567.exception;
 
 public class UserExistsException extends RegisterException {
-    private static final String msg = "Такой пользователь уже существует";
+    private static final String msg = "Такой email уже зарегистрирован";
 
     public UserExistsException() {
         super(msg);
