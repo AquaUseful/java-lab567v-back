228926b13ff36a64cc75fba7f89f41de83a1e67f
AquaUseful <l.vladik.m@gmail.com>
2023-02-01 23:44:59 +0300

    add: News model

---
+++ src/main/java/org/psu/lab567/model/News.java
@@ -0,0 +1,39 @@
+package org.psu.lab567.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class News {
+    public News(@NonNull String title, @NonNull String content, BinFile picture) {
+        this.title = title;
+        this.content = content;
+        this.picture = picture;
+    }
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @OneToOne(optional = true)
+    @JoinColumn
+    private BinFile picture;
+}
