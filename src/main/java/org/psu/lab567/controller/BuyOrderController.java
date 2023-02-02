e0ae6183fa79bca616b07188ae10d0c243155bb1
AquaUseful <l.vladik.m@gmail.com>
2023-02-02 13:53:26 +0300

    add: Order replacing ad deleting functionality

--- src/main/java/org/psu/lab567/controller/BuyOrderController.java
+++ src/main/java/org/psu/lab567/controller/BuyOrderController.java
@@ -12,8 +12,12 @@ import org.psu.lab567.service.BuyOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +44,28 @@ public class BuyOrderController {
         final User self = authService.getSelf();
         buyOrderService.fromRequest(request, self);
         return ResponseEntity.status(HttpStatus.CREATED).body(null);
+    }
+
+    @PutMapping(path = "{id}")
+    public ResponseEntity<Void> patchOrder(@NonNull @PathVariable("id") Long id,
+            @NonNull @RequestBody @Valid NewBuyOrderRequest request) {
+        final User self = authService.getSelf();
+        if (buyOrderService.isAuthorById(id, self)) {
+            buyOrderService.patchById(id, request);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+        }
+    }
+
+    @DeleteMapping(path = "{id}")
+    public ResponseEntity<Void> deleteOrder(@NonNull @PathVariable("id") Long id) {
+        final User self = authService.getSelf();
+        if (buyOrderService.isAuthorById(id, self)) {
+            buyOrderService.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+        }
     }
 }
--- src/main/java/org/psu/lab567/service/BuyOrderService.java
+++ src/main/java/org/psu/lab567/service/BuyOrderService.java
@@ -1,5 +1,7 @@
 package org.psu.lab567.service;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.psu.lab567.model.BuyOrder;
 import org.psu.lab567.model.User;
 import org.psu.lab567.pojo.NewBuyOrderRequest;
@@ -14,6 +16,12 @@ public class BuyOrderService {
     @Autowired
     private BuyOrderRepository buyOrderRepository;
 
+    public BuyOrder getById(@NonNull Long id) {
+        final BuyOrder order = buyOrderRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException());
+        return order;
+    }
+
     public void save(@NonNull BuyOrder order) {
         buyOrderRepository.save(order);
     }
@@ -25,5 +33,22 @@ public class BuyOrderService {
                 request.getComment(),
                 author);
         buyOrderRepository.save(order);
+    }
+
+    public void patchById(@NonNull Long id, @NonNull NewBuyOrderRequest patch) {
+        BuyOrder order = this.getById(id);
+        order.setProduct(patch.getProduct());
+        order.setAddress(patch.getAddress());
+        order.setComment(patch.getComment());
+        buyOrderRepository.save(order);
+    }
+
+    public boolean isAuthorById(@NonNull Long orderId, @NonNull User authorToTest) {
+        final BuyOrder order = getById(orderId);
+        return order.getAuthor().equals(authorToTest);
+    }
+
+    public void deleteById(@NonNull Long id) {
+        buyOrderRepository.deleteById(id);
     }
 }
